cdcb40457ca312d6910d76f7836bd61bc5d0b1c3
mygoaway <mygoaway2@gmail.com>
2022-08-20 13:39:04 +0900

    Initial commit

---
+++ src/main/java/inflearn/spring/security/SecurityController.java
@@ -0,0 +1,12 @@
+package inflearn.spring.security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityController {
+    @GetMapping("/")
+    public String index() {
+        return "Home";
+    }
+}
