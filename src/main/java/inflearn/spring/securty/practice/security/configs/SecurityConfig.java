60b6501f50bca6278aaba5d6dd23238d5b990375
mygoaway <mygoaway2@gmail.com>
2022-09-03 11:54:46 +0900

    실전프로젝트 1 - SecurityConfig 설정 및 HomeController 추가

---
+++ src/main/java/inflearn/spring/securty/practice/security/configs/SecurityConfig.java
@@ -0,0 +1,55 @@
+package inflearn.spring.securty.practice.security.configs;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        // js / css / image 파일 등 보안 필터를 적용할 필요가 없는 리소스를 설정 (보안 필터를 안거침)
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // 사용자 계정 생성 및 권한 부여
+        String password = passwordEncoder().encode("1111");
+        auth.inMemoryAuthentication().withUser("user").password(password).roles("USER");
+        auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER", "USER");
+        auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN", "USER", "MANAGE");
+    }
+
+    // 패스워드 암호화하는 작업(비밀번호를 안전하게 암호화 하도록 제공)
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        // 암호화 포맷 : {id}encodedPassword
+        // 기본 포맷은 Bcrypt : {bcrypt}$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HZWzG3YB1tlRy.fqvM/BG
+        // 알고리즘 종류 : Bcrypt, sha256 등
+        // PasswordEncoder 인터페이스를 구현해서 자기만의 패스워드 암호화하는 작업이 가능하다. encode(패스워드) : 패스워드 암호화 / matches(rawPassword, encoderPassword) : 패스워드 비교
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/users").permitAll()
+                .antMatchers("/mypage").hasRole("USER")
+                .antMatchers("/messages").hasRole("MANAGE")
+                .antMatchers("/config").hasRole("ADMIN")
+                .anyRequest().authenticated()
+
+        .and()
+                .formLogin();
+    }
+}
