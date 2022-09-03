ec126eabdc28a3471d07a23bcd780b67c48d84e7
mygoaway <mygoaway2@gmail.com>
2022-09-03 11:55:22 +0900

    실전프로젝트 1 - HomeController 추가

---
+++ src/main/java/inflearn/spring/securty/practice/controller/HomeController.java
@@ -0,0 +1,20 @@
+package inflearn.spring.securty.practice.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping(value="/")
+	public String home() throws Exception {
+		return "home";
+	}
+
+	@GetMapping(value="/login")
+	public String login() throws Exception {
+		return "login";
+	}
+
+}
