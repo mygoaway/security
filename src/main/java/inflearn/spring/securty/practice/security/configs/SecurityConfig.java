b0b74e65caeffbb161fc25b2758f5eb0d1f43fe7
mygoaway <mygoaway2@gmail.com>
2022-09-04 20:34:07 +0900

    실전프로젝트 2 - 로그인, 로그아웃, 부가기능, 성공/실패 핸들러, 인증거부 처리

--- src/main/java/inflearn/spring/securty/practice/security/configs/SecurityConfig.java
+++ src/main/java/inflearn/spring/securty/practice/security/configs/SecurityConfig.java
@@ -1,11 +1,15 @@
 package inflearn.spring.securty.practice.security.configs;
 
+import inflearn.spring.securty.practice.security.handler.CustomAccessDeniedHandler;
+import inflearn.spring.securty.practice.security.handler.CustomAuthenticationFailureHandler;
+import inflearn.spring.securty.practice.security.handler.CustomAuthenticationSuccessHandler;
 import inflearn.spring.securty.practice.security.provider.CustomAuthenticationProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationDetailsSource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -14,11 +18,16 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.AccessDeniedHandler;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // private final AuthenticationDetailsSource authenticationDetailsSource;
+    private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
+    private final CustomAuthenticationFailureHandler customAuthenticationFailureHandler;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -50,13 +59,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/users").permitAll()
+                .antMatchers("/", "/users", "user/login/**", "/login**").permitAll()
                 .antMatchers("/mypage").hasRole("USER")
                 .antMatchers("/messages").hasRole("MANAGE")
                 .antMatchers("/config").hasRole("ADMIN")
                 .anyRequest().authenticated()
 
         .and()
-                .formLogin();
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/login_proc")
+                //.authenticationDetailsSource(authenticationDetailsSource) // 아이디, 패스워드 이외 추가정보 저징
+                .successHandler(customAuthenticationSuccessHandler)
+                .failureHandler(customAuthenticationFailureHandler)
+                .defaultSuccessUrl("/")
+                .permitAll()
+        .and()
+                .exceptionHandling()
+                .accessDeniedHandler(accessDeniedHandler())
+        ;
+    }
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        CustomAccessDeniedHandler accessDeniedHandler = new CustomAccessDeniedHandler();
+        accessDeniedHandler.setErrorPage("/denied");
+        return accessDeniedHandler;
     }
 }
--- src/main/java/inflearn/spring/securty/practice/security/provider/CustomAuthenticationProvider.java
+++ src/main/java/inflearn/spring/securty/practice/security/provider/CustomAuthenticationProvider.java
@@ -1,7 +1,6 @@
 package inflearn.spring.securty.practice.security.provider;
 
 import inflearn.spring.securty.practice.security.service.AccountContext;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -10,7 +9,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
 
 public class CustomAuthenticationProvider implements AuthenticationProvider {
 
@@ -32,6 +30,14 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
         if(!passwordEncoder.matches(password, accountContext.getAccount().getPassword())) {
             throw new BadCredentialsException("Invalid password");
         }
+
+        /*
+        FormWebAuthenticationDetails details = (FormWebAuthenticationDetails)authentication.getDetails();
+        String secretKey = details.getSecretKey();
+        if(secretKey == null || !"secret".equals(details)) {
+            throw new InsufficientAuthenticationException("InsufficientAuthenticationException");
+        }
+        */
 
         return new UsernamePasswordAuthenticationToken(accountContext.getAccount(), null, accountContext.getAuthorities());
     }
